df796e7e245ecf5ecf3856e6cd076950fb20a6e1
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2021-12-14 22:48:43 +0100

    updating shop

--- screens/shop/ProductOverview.js
+++ screens/shop/ProductOverview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList,Text } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductOverview = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -8,7 +9,7 @@ const ProductOverview = (props) => {
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => <ProductItem image={item.data.imageUrl} price={item.data.title} title={item.data.title}/>}
     />
   );
 };
